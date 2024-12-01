305cf2acbd11b4b5a2a9a7eafd144250601af7ce
Leandro Licata <leandrolicata1@gmail.com>
2024-12-01 13:27:56 -0300

    navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isCollapsed = true;
+  isMobile = false
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', [])
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768; // Cambia el tamaño si es necesario
+    if (!this.isMobile) {
+      this.isCollapsed = false; // Siempre visible en desktop
+    } else {
+      this.isCollapsed = true; // Colapsado en mobile
+    }
+  }
+
+  toggleNavbar() {
+    if (this.isMobile) {
+      this.isCollapsed = !this.isCollapsed; // Solo afecta en mobile
+    }
+  }
 }
